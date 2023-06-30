d54c7728351bf910ed2e8d857f46d69bbbe23795
LinuxBeaver <78667207+LinuxBeaver@users.noreply.github.com>
2023-06-30 16:21:14 +0000

    Add files via upload

--- rocktext2.c
+++ rocktext2.c
@@ -17,6 +17,29 @@
  * 2023 Beaver (GEGL Rock Text 2) 
  */
 
+/*
+
+GEGL Graph of Rock Text 2 so users can test without installing. REQUIRES LONG SHADOW PD AND CUSTOM BEVEL
+
+#rock text2
+median-blur radius=2 alpha-percentile=80
+noise-spread amount-x=38 amount-y=48 seed=233
+gimp:threshold-alpha
+custom-bevel blendmode=Hardlight  gaus=4 box=4 alphapercentile=10
+gimp:threshold-alpha
+bloom strength=10
+id=ls
+gimp:layer-mode layer-mode=behind aux=[ ref=ls long-shadow-pd angle=90 length=9 lightness=99  ]
+crop
+denoise-dct patch-size=size8x8 sigma=7
+id=slemboss
+gimp:layer-mode opacity=0.50 layer-mode=softlight aux=[ ref=slemboss emboss depth=10 elevation=60 azimuth=120 ]
+levels in-low=0.09
+brightness-contrast brightness=0.04
+noise-reduction iterations=2
+ */
+
+
 #include "config.h"
 #include <glib/gi18n-lib.h>
 
@@ -38,21 +61,12 @@ property_int    (amounty, _("Rockification Vertical"), 22)
 
 property_seed (seed, _("Random seed"), rand)
 
-
-property_string (string1, _("Graph1"), TUTORIAL1)
-    ui_meta     ("role", "output-extent")
-
 #define TUTORIAL1 \
 " gimp:threshold-alpha bloom strength=10 "\
 
 
-property_string (string2, _("Graph2"), TUTORIAL2)
-    ui_meta     ("role", "output-extent")
-
 #define TUTORIAL2 \
 " crop denoise-dct patch-size=size8x8 sigma=8 id=slemboss gimp:layer-mode opacity=0.50 layer-mode=softlight aux=[ ref=slemboss emboss depth=10 elevation=60 azimuth=120 ] noise-reduction iterations=2 "\
-
-
 
 enum_start (gegl_blend_mode_typecbevel3)
   enum_value (GEGL_BLEND_MODE_TYPE_HARDLIGHT3, "Hardlight",
@@ -84,26 +98,23 @@ property_enum (blendmode3, _("Blend Mode of Internal Emboss"),
     GEGL_BLEND_MODE_TYPE_HARDLIGHT3)
 
 
-property_int (depth, _("Bevel Depth (makes darker)"), 100)
-    description (_("Filter width"))
+property_int (depth, _("Bevel Depth and or Detail"), 100)
+    description (_("Depending on the blend mode this will either add depth and or bring out details"))
     value_range (10, 100)
 
 property_double (gaus, _("Internal Gaussian Blur (fatness of bevel)"), 5)
-   description (_("Standard deviation for the XY axis"))
+   description (_("Gaussian Blur makes a normal fat bevel"))
    value_range (0.0, 5.0)
 
 
 
-property_int (box, _("Internal Box Blur (fatness of bevel)"), 1)
-   description(_("Radius of square pixel region, (width and height will be radius*2+1)"))
+property_int (box, _("Internal Box Blur (makes sharper bevel)"), 1)
+   description(_("Box Blur makes a sharper bevel"))
    value_range (0, 6)
    ui_range    (0, 6)
    ui_gamma   (1.5)
 
-
-
-
-property_int  (radius, _("Radius (Low smaller - large bigger)"), 2)
+property_int  (radius, _("Median's Radius"), 2)
   value_range (-40, 40)
   ui_range    (-40, 40)
   ui_meta     ("unit", "pixel-distance")
@@ -119,10 +130,10 @@ property_double  (alphapercentile, _("Median's Alpha Percentile 2 (Wideness cont
   description (_("Neighborhood alpha percentile"))
 
 property_color (coloroverlay, _("Color Overlay (uses Multiply blend mode)"), "#ffffff")
-    description (_("The color to paint over the input"))
+    description (_("The color to paint over the rock text. If the color is white or grayscale this will change it to any color"))
 
 property_double (inlow, _("Lightness to Darkness meter"), 0.09)
-    description ( _("Input luminance level to become lowest output"))
+    description ( _("Levels in Low for lightness"))
     ui_range    (0.0, 0.25)
 
 
@@ -131,25 +142,20 @@ property_file_path(layer, _("Image file overlay (color white recommended)"), "")
     description (_("Source image file path (png, jpg, raw, svg, bmp, tif, ...)"))
 
 property_double (angle, _("Angle of Extrusion"), 72.0)
-  description (_("Shadow angle"))
+  description (_("LongShadow angle"))
   value_range (-180.0, 180.0)
   ui_meta     ("unit", "degree")
   ui_meta     ("direction", "cw")
 
 property_double (length, _("Length of Extrusion"), 23)
-  description (_("Shadow length"))
+  description (_("LongShadow length"))
   value_range (0, 55)
   ui_range    (0, 55)
 
 
-
-
 property_double (lightness, _("Darkness to Lightness of Extrusion"), 2.4)
-   description  (_("Lightness adjustment"))
+   description  (_("Lightness adjustment for the LongShadow"))
    value_range  (-30.0, 30.0)
-
-
-
 
 #else
 
@@ -162,7 +168,7 @@ property_double (lightness, _("Darkness to Lightness of Extrusion"), 2.4)
 static void attach (GeglOperation *operation)
 {
   GeglNode *gegl = operation->node;
-  GeglNode *input, *median, *spread, *ta, *cb, *nop, *graph1, *levels, *behind, *lspd, *inlow, *color, *multiply, *graph2, *output;
+  GeglNode *input, *median, *spread, *ta, *cb, *nop, *graph1, *levels, *multiply, *behind, *lspd,  *color,  *graph2, *output;
 
   input    = gegl_node_get_input_proxy (gegl, "input");
   output   = gegl_node_get_output_proxy (gegl, "output");
@@ -198,11 +204,11 @@ static void attach (GeglOperation *operation)
                                   NULL);
 
   graph1    = gegl_node_new_child (gegl,
-                                  "operation", "gegl:gegl",
+                                  "operation", "gegl:gegl", "string", TUTORIAL1,
                                   NULL);
 
   graph2    = gegl_node_new_child (gegl,
-                                  "operation", "gegl:gegl",
+                                  "operation", "gegl:gegl", "string", TUTORIAL2,
                                   NULL);
 
   ta    = gegl_node_new_child (gegl,
@@ -217,11 +223,7 @@ static void attach (GeglOperation *operation)
                                   "operation", "gegl:levels",
                                   NULL);
                         
-                        
-                        
-                        
-                        
-                        
+
   gegl_operation_meta_redirect (operation, "value", color, "value");
   gegl_operation_meta_redirect (operation, "gaus", cb, "gaus");
   gegl_operation_meta_redirect (operation, "box", cb, "box");
@@ -242,12 +244,11 @@ static void attach (GeglOperation *operation)
   gegl_operation_meta_redirect (operation, "lightness", lspd, "lightness");
   gegl_operation_meta_redirect (operation, "inlow", levels, "in-low");
 
-
-
-
-  gegl_node_link_many (input, median, spread, ta, cb, graph1, nop, behind, graph2, levels, output, NULL);
+  gegl_node_link_many (input, median, spread, ta, cb, graph1, nop, behind, graph2, levels, multiply, output, NULL);
   gegl_node_link_many (nop, lspd, NULL);
   gegl_node_connect_from (behind, "aux", lspd, "output");
+  gegl_node_connect_from (multiply, "aux", color, "output");
+
 
 
 }
@@ -266,7 +267,7 @@ gegl_op_class_init (GeglOpClass *klass)
     "title",       _("Rock Text 2"),
     "categories",  "Aristic",
     "reference-hash", "b2do6a2ro26rogk0fjf25sb2ac",
-    "description", _("GEGL generates specialized beveled text like a rock. Filter Requires the user to use their image file overlay texture. Textures provided in rock text repo. This filter has two required dependency filters; custom-bevel and extrusion 2. Both are useful filters on their own and are required for rock text 2 to work. "
+    "description", _("GEGL generates specialized beveled text like a rock. Filter HIGHLY RECOMMENDS the user to use their image file overlay texture. Textures provided in stone text 2 repo. This filter has two required dependency filters; custom-bevel and extrusion 2. Both are useful filters on their own and are required for rock text 2 to work. "
                      ""),
     NULL);
 }
